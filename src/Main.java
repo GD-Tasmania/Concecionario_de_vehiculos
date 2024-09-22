2cab7680f291985b1a5049a2ed5b9bcc199481d7
Jorge Valencia <Jorge591vl@gmail.com>
2024-09-21 21:32:45 -0500

    feat: Se crearon metodos para crear, buscar, actualizar y eliminar clientes.T23

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import Service.Concecionario;
+import Model.*;
 
 public class Main {
-
+	public static Concecionario c = new Concecionario();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		
+		Cliente cliente = new Cliente("Tito", "SENA", "3218056116", "TITO23@GMAIL.COM");
+		c.agregarCliente(cliente);
+		c.buscarCliente("Tito", "3218056116");
+		Cliente lucho = c.buscarCliente("Tito", "3218056116");
+		c.actualizarCliente("lucho", "UNIVERSIDAD", "3207787962", "LUCHO2@gmail.com", lucho);
+		c.eliminarCliente("lucho", "3207787962");
 	}
-
 }
--- src/Service/Concecionario.java
+++ src/Service/Concecionario.java
@@ -13,4 +13,45 @@ public class Concecionario {
 	private ArrayList<Vehiculo> vehiculo;
 	private ArrayList<Inventario> inventarios;
 	private ArrayList<Mantenimiento> mantenimientos;
+	
+	public Concecionario() {
+		this.clientes = new ArrayList<>();
+		this.ventas = new ArrayList<>();
+		this.vehiculo = new ArrayList<>();
+		this.inventarios = new ArrayList<>();
+		this.mantenimientos = new ArrayList<>();
+	}
+	//METODOS CLIENTES
+	public void agregarCliente(Cliente cliente) {
+		clientes.add(cliente);
+		System.out.println("se agregó cliente");
+	}
+	
+	public Cliente buscarCliente(String nombre, String telefono) {
+		for (Cliente cliente : this.clientes) {
+			if(cliente.getNombre().equalsIgnoreCase(nombre) && cliente.getTelefono().equalsIgnoreCase(telefono)){
+				System.out.println(cliente.toString());
+				return cliente;
+			}
+		}
+		return null;
+	}
+	
+	public void eliminarCliente(String nombre, String telefono) {
+		Cliente cliente = buscarCliente(nombre, telefono);
+		if(cliente != null) {
+			this.clientes.remove(cliente);
+			System.out.println(this.clientes);
+		}
+	}
+	
+	public void actualizarCliente(String nombre, String direccion, String telefono, String correo, Cliente cliente) {
+		if(cliente != null) {
+			cliente.setNombre(nombre);	
+			cliente.setDireccion(direccion);	
+			cliente.setTelefono(telefono);	
+			cliente.setCorreo(correo);	
+		}
+		System.out.println(cliente.toString());
+	}
 }
